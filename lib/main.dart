2cc2fbcd7d4a55a289e372a8df98fe4477120751
galliume <gcconantc@gmail.com>
2020-02-19 22:25:12 +0100

    change archi

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,4 @@
 import 'package:flutter/material.dart';
+import 'src/App.dart';
 
-void main() => runApp(BookSwapApp());
-
-class BookSwapApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Book Swap',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: HomePage(title: 'Swap locally your books'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-          ],
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(App());
